7cc006a530086a1be6dce85eb89baef779b7f156
PENG GAO <gaopeng19931027@gmail.com>
2018-10-08 18:44:01 +1100

    Ecshop update

---
+++ ecshop/h5/static/config.js
@@ -0,0 +1,10 @@
+const env = {  
+  'API_HOST': 'http://api.localhost',
+  // 'API_HOST': 'http://api.localhost',
+  'APP_NAME': 'Demo',
+  'APP_DESC': 'This is a  demo',
+  'APP_KEYWORDS': 'Demo, Shop',
+  'DEBUG': false,
+  'ENCRYPTED': false,
+  'VERSION': '1.0.0', // API版本号
+}
